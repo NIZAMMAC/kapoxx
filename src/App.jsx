40911cecdc921e2fb48772e49ae7cd2bfe4278aa
42dh558p9g-cmd <42dh558p9g@privaterelay.appleid.com>
2026-06-28 18:59:48 +0530

    Install Lenis and add Apple-like global smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -7,6 +11,29 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Apple-like easing
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <div className="app-container">
       <Navbar />
